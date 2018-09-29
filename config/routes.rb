f03725fccb949850b9d3b9c657966b03e708629d
Felice Forby <feliceforby@gmail.com>
2018-09-29 01:19:27 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts, only: %i[index show new create edit update]
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+
+  # more explicit options for edit and update:
+  # get 'posts/:id/edit', to: 'post#edit', as: edit_post
+  # patch 'posts/:id', to: 'post#update'
 end
